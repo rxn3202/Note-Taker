ff2992688084ca1a6d49df1c8579e9220a925070
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-04 15:26:59 -0700

    added html routes to server

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
+const path = require('path');
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
