55ec80fd0c2df917bf9583ee9519d948a2f2050c
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-04 17:58:37 -0700

    cleaned up code

--- server.js
+++ server.js
@@ -1,39 +1,64 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
-
 const path = require('path');
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-app.get('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    res.json(notes);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
-app.post('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    let newNote = req.body;
-    newNote.id = uuidv4();
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
     notes.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-    res.json(notes);
+    
+    fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), err => {
+      if (err) throw err;
+      res.status(200).json(newNote);
+    });
+  });
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    notes = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-    res.json(notes);
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter(note => note.id !== noteId);
+    
+    fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), err => {
+      if (err) throw err;
+      res.status(200).end();
+    });
+  });
+});
+
+// HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
 });
